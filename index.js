b8936d65da7ead55315cfaec178fd972d17a4782
aaki11 <khandelwal.aakash91@gmail.com>
2020-05-22 19:42:45 +0530

    Bare  Bone Redux Implementation

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-// Import stylesheets
-import './style.css';
+function createStore(reducer, initialState) {
+  let reducerFunction = reducer;
+  let currentState = initialState;
+  let listener = () => {};
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+  return {
+    getState() {
+      return currentState;
+    },
+    dispatch(action) {
+      currentState = reducerFunction(currentState, action);
+      listener();
+      return action;
+    },
+    subscribe(newListener) {
+      listener = newListener;
+    }
+  };
+}
+
+/*test example*/
+function counter(state = 0, action) {
+  let { type } = action;
+  switch (type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+let store = createStore(counter);
+
+store.subscribe(() => {
+  alert(store.getState());
+});
+
+console.log("store", store);
+
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
+store.dispatch({ type: "INCREMENT" });
